db21919b36a71130e46e79189e55f41edccbb21b
teemago <65630024+teemago@users.noreply.github.com>
2023-11-07 03:09:39 -0300

    map add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const submit = document.getElementById("button");
 const verificar = document.getElementById("verificar");
+const map = document.getElementById("map");
 
 submit.addEventListener("click", async () => {
   const name = document.getElementById("name");
@@ -25,6 +26,10 @@ submit.addEventListener("click", async () => {
 
 verificar.addEventListener("click", async () => {
   if (cep.value !== "") {
+    let cepSearch = cep.value;
+    if (cep.value.indexOf("-") === -1) {
+      cepSearch = cep.value.slice(0, 5) + "-" + cep.value.slice(5);
+    }
     await axios
       .get(`https://viacep.com.br/ws/${cep.value}/json/`)
       .then((res) => {
@@ -36,10 +41,20 @@ verificar.addEventListener("click", async () => {
         estado.innerHTML = res.data.uf;
         cidade.innerHTML = res.data.localidade;
         bairro.innerHTML = res.data.bairro;
-        if(res.data.logradouro){
-            rua.innerHTML = res.data.logradouro + ", " + res.data.complemento;
+        if (res.data.logradouro) {
+          rua.innerHTML = res.data.logradouro + ", " + res.data.complemento;
         }
       })
       .catch((err) => alert("Escreva o CEP correto"));
+    axios
+      .get(
+        `https://api.geoapify.com/v1/geocode/search?postcode=${cepSearch}&format=json&apiKey=8cfc9ea2ee744a29a7dc7d0475a05d09
+      `
+      )
+      .then((res) => {
+        console.log(res);
+        map.setAttribute("src", `https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=1400&height=600&center=lonlat:${res.data.results[0].lon},${res.data.results[0].lat}&zoom=14.4367&scaleFactor=2&apiKey=8cfc9ea2ee744a29a7dc7d0475a05d09`)
+      })
+      .catch((err) => console.log(err));
   }
 });
